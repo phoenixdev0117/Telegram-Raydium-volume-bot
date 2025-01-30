c57cdbbc7efd167ef026b63f51ee63d70cdafed8
Phoenix Dev🐦 <55408180+phoenixdev0117@users.noreply.github.com>
2025-01-29 22:54:19 -0800

    fix: update menu
    Co-Authored-By: whalesharka1129 <192328598+whalesharka1129@users.noreply.github.com>
    Co-Authored-By: Spirit-crypto <bluesoul2130758@gmail.com>
    Co-Authored-By: NS0126 <191113631+NS0126@users.noreply.github.com>
    Co-Authored-By: FIREADY126 <fiready1234@gmail.com>

--- menu/menu.ts
+++ menu/menu.ts
@@ -18,3 +18,14 @@ export const main_menu_display = () => {
     console.log('\t[6] - Gather sol from new wallets');
     console.log('\t[7] - Exit');
 }
+
+export const settings_display = () => {
+    console.log('\t[1] - Contract Address of the token');
+    console.log('\t[2] - Pool Address of the token pair');
+    console.log('\t[3] - Maximum amount of Solana to buy');
+    console.log('\t[4] - Minimum amount of Solana to buy');
+    console.log('\t[5] - Number of wallets to newly create');
+    console.log('\t[6] - Time Interval between each wallets in miliseconds');
+    console.log('\t[7] - Back');
+    console.log('\t[8] - Exit');
+}
